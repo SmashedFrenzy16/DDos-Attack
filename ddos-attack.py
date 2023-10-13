19b57297d0d6d93a6c6c39d05c5a46280e86af19
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2023-10-13 10:56:58 +0100

    Update ddos-attack.py with another f-string

--- ddos-attack.py
+++ ddos-attack.py
@@ -29,7 +29,7 @@ animation = ["[■□□□□□□□□□]","[■■□□□□□□□□
 
 for i in range(len(animation)):
     time.sleep(0.2)
-    sys.stdout.write("\r" + animation[i % len(animation)])
+    sys.stdout.write(f"\r{animation[i % len(animation)]}")
     sys.stdout.flush()
 
 print("\n")
